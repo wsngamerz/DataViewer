1e91712294f00ee828c5f8184faf2801ea907afd
William Neild <wneild5@gmail.com>
2025-11-12 22:37:15 +0000

    feat: add oapi schema generation

--- backend/internal/server/inject.go
+++ backend/internal/server/inject.go
@@ -1,6 +1,10 @@
 package server
 
 import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humagin"
 	"github.com/gin-contrib/cors"
@@ -27,5 +31,44 @@ func inject(ds *DataSources, cfg *config.Config, _ externalClients) (*gin.Engine
 
 	public := router.Group("")
 	health.NewHandler(public.Group("/health"))
+
+	if err := exportOpenAPI(api.OpenAPI()); err != nil {
+		return nil, err
+	}
 	return router, nil
+}
+
+func exportOpenAPI(api *huma.OpenAPI) error {
+	log.Info().Msg("Exporting OpenAPI specification")
+	rawJSON, err := api.MarshalJSON()
+	if err != nil {
+		return err
+	}
+
+	var obj interface{}
+	if err := json.Unmarshal(rawJSON, &obj); err != nil {
+		return err
+	}
+
+	prettyJSON, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll("docs", 0755); err != nil {
+		return err
+	}
+
+	openapiJSONPath := filepath.Join("docs", "openapi.json")
+	err = os.WriteFile(openapiJSONPath, prettyJSON, 0644)
+	if err != nil {
+		return err
+	}
+
+	openapiYAMLPath := filepath.Join("docs", "openapi.yaml")
+	openapiYAML, err := api.YAML()
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(openapiYAMLPath, openapiYAML, 0644)
 }
